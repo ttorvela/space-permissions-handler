44b18cc3f48844f448382050df0214dae7832cb0
ttorvela <thimee@users.sourceforge.net>
2014-07-30 22:51:16 +0300

    Dirty UI
    Dirty UI added

--- src/main/resources/js/admin.js
+++ src/main/resources/js/admin.js
@@ -1,29 +1,35 @@
-	function getPermissions() {
-		return AJS.$.ajax({
-			url : baseUrl + "/rest/userpermissions/1.0/"
-					+ AJS.$("#usernameFrom").attr("value"),
-			dataType : "json"
-		});
-	}
+function getPermissions() {
+	return AJS.$.ajax({
+		url : baseUrl + "/rest/userpermissions/1.0/"
+				+ AJS.$("#usernameFrom").attr("value"),
+		dataType : "json"
+	});
+}
 
-	function copyPermissions(permissions) {
-		AJS.$.ajax({
-			url : baseUrl + "/rest/userpermissions/1.0/"
-					+ AJS.$("#usernameTo").attr("value"),
-			type : "PUT",
-			contentType : "application/json",
-			data : JSON.stringify(permissions),
-			processData : false
-		});
-	}
+function copyPermissions(permissions) {
+	AJS.$.ajax({
+		url : baseUrl + "/rest/userpermissions/1.0/"
+				+ AJS.$("#usernameTo").attr("value"),
+		type : "PUT",
+		contentType : "application/json",
+		data : JSON.stringify(permissions),
+		processData : false
+	});
+}
 
 AJS.toInit(function() {
-	AJS.$("#admin").submit(function(e) {
-		e.preventDefault();
+	AJS.$("#admin").submit(
+			function(e) {
+				e.preventDefault();
 
-		var promise = getPermissions();
-		promise.success(function (permissions) {
-			copyPermissions(permissions);
-		});
-	});
+				var promise = getPermissions();
+				promise.success(function(permissions) {
+					copyPermissions(permissions);
+					var $permissionResults = $("#results");
+					$permissionResults.append("<h2 id='permissions'>Permissions copied</h2>");
+					$permissionResults.append("<div><pre>"
+							+ JSON.stringify(permissions, null, 4)
+							+ "</pre></div>");
+				});
+			});
 });
