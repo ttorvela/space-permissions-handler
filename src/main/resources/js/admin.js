8cb9636e975fe6bc5f64512533afd5362a659cd0
ttorvela <thimee@users.sourceforge.net>
2015-03-23 22:51:27 +0200

    Finalize printing to template.soy
    Cleaned up admin.js from permission printing and added it to
    template.soy

--- src/main/resources/js/admin.js
+++ src/main/resources/js/admin.js
@@ -32,161 +32,6 @@ function copyPermissions(permissions, onlyUserPermissions) {
      });
 }
 
-function printHeader(permissions) {
-	var $permissionResults = $("#results");
-	var headerText = 
-	$permissionResults.html("<h2 id='permissionsHeader'>" + AJS.escapeHtml(AJS.$("#usernameFrom").attr("value")) + "'s permissions in "
-			+ permissions.spacePermissions.length + " spaces"
-			+ ". Group permissions are marked with *."
-			+ "</h2>");
-
-	var html = "";
-	var ver5 = versionNumber.substring(0, 1) == "5";
-	
-	if (ver5) {
-		html += '<table class="permissions aui" id="uPermissionsTable" width="100%" cellspacing="0" cellpadding="2" border="0">';
-	} else {
-		html += '<table id="uPermissionsTable" width="100%" cellspacing="0" cellpadding="2" border="0">';
-	}
-
-	html += '<tbody>';
-	html += '<tr>';
-	html += '<th width="25%" class="permissionHeading">&nbsp;</th>';
-		
-	
-	if (ver5) {
-		html += '<th class="permissionHeading">All</th>';
-		html += '<th class="permissionSuperTab" colspan="3">Pages</th>';
-	} else {
-		html += '<th class="permissionHeading">&nbsp;</th>';
-		html += '<th class="permissionSuperTab" colspan="4">Pages</th>';
-	}
-	
-	
-	html += '<th class="permissionSuperTab" colspan="2">Blog</th>';
-	html += '<th class="permissionSuperTab" colspan="2">Comments</th>';
-	html += '<th class="permissionSuperTab" colspan="2">Attachments</th>';
-	html += '<th class="permissionSuperTab">Mail</th>';
-	html += '<th class="permissionSuperTab" colspan="2">Space</th>';
-	html += '</tr>';
-	html += '<tr>';
-	html += '<th class="permissionHeading">Space name</th>';
-	html += '<th width="40" class="permissionSuperTab">View</th>';
-	
-	if (ver5) {
-		html += '<th width="40" class="permissions-group-start">Add</th>';
-		html += '<th width="40">Restrict</th>';
-		html += '<th width="40">Delete</th>';
-		html += '<th width="40" class="permissions-group-start">Add</th>';
-		html += '<th width="40">Delete</th>';
-		html += '<th width="40" class="permissions-group-start">Add</th>';
-		html += '<th width="40">Delete</th>';
-		html += '<th width="40" class="permissions-group-start">Add</th>';
-		html += '<th width="40">Delete</th>';
-		html += '<th width="40" class="permissions-group-start">Delete</th>';
-		html += '<th width="40" class="permissions-group-start">Export</th>';
-		html += '<th width="40">Admin</th>';
-	} else {
-		html += '<th width="40" class="permissionTab">Add</th>';
-		html += '<th width="40" class="permissionTab">Export</th>';
-		html += '<th width="40" class="permissionTab">Restrict</th>';
-		html += '<th width="40" class="permissionTab">Remove</th>';
-		html += '<th width="40" class="permissionTab">Add</th>';
-		html += '<th width="40" class="permissionTab">Remove</th>';
-		html += '<th width="40" class="permissionTab">Add</th>';
-		html += '<th width="40" class="permissionTab">Remove</th>';
-		html += '<th width="40" class="permissionTab">Add</th>';
-		html += '<th width="40" class="permissionTab">Remove</th>';
-		html += '<th width="40" class="permissionTab">Remove</th>';
-		html += '<th width="40" class="permissionTab">Export</th>';
-		html += '<th width="40" class="permissionTab">Admin</th>';
-	}
-	
-	html += '</tr>';
-
-	return html;
-}
-
-function printPermissions(permissions) {
-	var html = "";
-	var ver5 = versionNumber.substring(0, 1) == "5";
-	
-	for (i = 0; i < permissions.spacePermissions.length; i++) {
-		if (i > 0 && i % 2 == 1) {
-			html += '<tr style=" background: #f0f0f0; " class="space-permission-row">';
-		} else {
-			html += '<tr class="space-permission-row">';
-		}
-		html += '<td> <a title="' + AJS.escapeHtml(permissions.spacePermissions[i].spaceName)
-			+ '" href="/confluence/spaces/spacepermissions.action?key='
-			+ AJS.escapeHtml(permissions.spacePermissions[i].spaceKey) + '">' 
-			+ AJS.escapeHtml(permissions.spacePermissions[i].spaceName) + '</a>' + '</td>';
-		
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[0].permissionGranted, "viewspace", permissions.spacePermissions[i].permissions[0].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[1].permissionGranted, "createpage", permissions.spacePermissions[i].permissions[1].userPermission);
-		if (!ver5) {
-			html += printPermissionStatus(permissions.spacePermissions[i].permissions[2].permissionGranted, "exportpage", permissions.spacePermissions[i].permissions[2].userPermission);
-		}
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[3].permissionGranted, "setpagepermissions", permissions.spacePermissions[i].permissions[3].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[4].permissionGranted, "removepage", permissions.spacePermissions[i].permissions[4].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[5].permissionGranted, "createblogpost", permissions.spacePermissions[i].permissions[5].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[6].permissionGranted, "removeblog", permissions.spacePermissions[i].permissions[6].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[7].permissionGranted, "comment", permissions.spacePermissions[i].permissions[7].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[8].permissionGranted, "removecomment", permissions.spacePermissions[i].permissions[8].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[9].permissionGranted, "createattachment", permissions.spacePermissions[i].permissions[9].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[10].permissionGranted, "removeattachment", permissions.spacePermissions[i].permissions[10].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[11].permissionGranted, "removemail", permissions.spacePermissions[i].permissions[11].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[12].permissionGranted, "exportspace", permissions.spacePermissions[i].permissions[12].userPermission);
-		html += printPermissionStatus(permissions.spacePermissions[i].permissions[13].permissionGranted, "editspace", permissions.spacePermissions[i].permissions[13].userPermission);
-		html += '</tr>';
-	}
-	
-	
-	html += '</tbody>';
-	html += '</table>';
-	
-	return html;
-}
-
-function printPermissionStatus(status, permissionName, userPermission) {
-	var ver5 = versionNumber.substring(0, 1) == "5";
-
-	var html = "";
-	
-	if (permissionName == "exportPage" && ver5) {
-		html = "";
-	} else {
-		html += '<td valign="middle" align="center" data-permission-set="' + status + '"';
-		html += 'data-permission-group="" data-permission="' + permissionName + '"';
-		html += 'class="permissionCell ';
-		
-		if (permissionName == "createpage" 
-			|| permissionName == "createblogpost" 
-				|| permissionName == "comment" 
-					|| permissionName == "createattachment" 
-						|| permissionName == "removemail" 
-							|| permissionName == "exportspace") {
-			html += 'permissions-group-start';
-		}
-
-		html += '"><img width="16" height="16" align="absmiddle" border="0" ';
-		
-		if (status == true) {
-			html += 'src="' + AJS.params.staticResourceUrlPrefix + '/images/icons/emoticons/check.png">';
-		} else {
-			html += 'src="' + AJS.params.staticResourceUrlPrefix + '/images/icons/emoticons/error.png">';
-		}
-		
-		if (userPermission == false && status == true) {
-			html += '*';
-		}
-		
-		html += '</td>';
-	}
-
-	return html;
-}
-
 function showSpinner() {
 	killSpinner = Raphael.spinner("throbber", 60, "#666");
 	$("#throbber").removeClass("hidden");
@@ -235,18 +80,12 @@ AJS.toInit(function() {
 							var $copyStatus= $("#copyStatus");
 							$copyStatus.html('');
 						}
-						
-						//var template = Confluence.Templates.Example.helloWorld({name: "John Smith"});
 
-//						var html = printHeader(permissions);
-//						html += printPermissions(permissions);
-						
-						//console.log(permissions);
 						var ver5 = versionNumber.substring(0, 1) == "5";
 						
 						var html = Confluence.Templates.SpacePermissionsHandler.listSpacePermissions(
 								{permissions: permissions, userFrom: AJS.$("#usernameFrom").attr("value"),
-								 version5: ver5});
+								 version5: ver5, emoticonsUrl: AJS.params.staticResourceUrlPrefix + "/images/icons/emoticons"});
 						
 						var $permissionResults = $("#results");
 						$permissionResults.append(html);
